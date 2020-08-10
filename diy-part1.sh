aa10057b1359825d87fddaa59470ab4c8db42c70
lj0307222 <61595433+lj0307222@users.noreply.github.com>
2020-08-11 00:00:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,6 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i 's/src-git packages https://github.com/coolsnowwolf/packages/src-git lienol https://github.com/Lienol/openwrt-package/' feeds.conf.default
+# sed -i 's/src-git packages https://github.com/coolsnowwolf/packages/src-git lienol https://github.com/Lienol/openwrt-package/' feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
